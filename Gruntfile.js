ac5db52c977240a1399744dbb778eedfbc4fbabb
supertassu <supertassu@users.noreply.github.com>
2016-01-14 16:01:49 +0200

    added travis and grunt better config

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,15 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'js/*.js', '*.js'],
+      files: ['Gruntfile.js', 'js/*.js', 'index.js'],
       options: {
         globals: {
           jQuery: true
         }
       }
+    },
+    nodeunit: {
+      all: ['index.js', "js/*.js"]
     },
     watch: {
       files: ['<%= jshint.files %>'],
@@ -17,7 +20,17 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   grunt.registerTask('default', ['jshint']);
+  //grunt.registerTask('test', ['jshint', 'nodeunit']);
+
+  grunt.registerTask('test', 'runs my tasks', function () {
+    var tasks = ['jshint', 'nodeunit'];
+
+    // Use the force option for all tasks declared in the previous line
+    grunt.option('force', true);
+    grunt.task.run(tasks);
+});
 
 };
